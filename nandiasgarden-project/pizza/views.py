1b528f0f8ff1f9299ae18ef344132e787bbbac30
Sagar Suman <32593284+sagar5suman@users.noreply.github.com>
2020-10-20 00:55:56 +0530

    Update views.py

--- nandiasgarden-project/pizza/views.py
+++ nandiasgarden-project/pizza/views.py
@@ -15,8 +15,11 @@ def order(request):
             created_pizza = filled_form.save()
             created_pizza_pk = created_pizza.pk
             note = f"Thanks for ordering! Your {filled_form.cleaned_data['size']} {filled_form.cleaned_data['topping1']} and {filled_form.cleaned_data['topping2']} pizza is on its way!"
-            new_form = PizzaForm()
-            return render(request, 'pizza/order.html', {'created_pizza_pk': created_pizza_pk,'pizzaform': new_form, 'note': note, 'multiple_form': multiple_form})
+            filled_form = PizzaForm()
+        else:
+            created_pizza_pk = None
+            note = 'Pizza order has failed. Try again.'
+        return render(request, 'pizza/order.html', {'created_pizza_pk': created_pizza_pk,'pizzaform': filled_form, 'note': note, 'multiple_form': multiple_form})
     else:
         form = PizzaForm()
         return render(request, 'pizza/order.html', {'pizzaform': form, 'multiple_form': multiple_form})

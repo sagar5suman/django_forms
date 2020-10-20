1f7411b5138916066a39a897880b816e01a27fb3
Sagar Suman <sagar9suman@gmail.com>
2020-10-20 17:55:20 +0530

    Update project

--- nandiasgarden-project/nandiasgarden/settings.py
+++ nandiasgarden-project/nandiasgarden/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'pizza',
+    'widget_tweaks',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
